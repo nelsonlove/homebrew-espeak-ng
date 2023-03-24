f76769a1ef1712e64add718a1812816f27fa0612
Nelson Love <nelson.love@l7informatics.com>
2023-03-23 20:37:13 -0400

    Update urls

--- Formula/espeak-ng.rb
+++ Formula/espeak-ng.rb
@@ -1,9 +1,9 @@
 class EspeakNg < Formula
   desc "Open source speech synthesizer"
-  homepage "https://github.com/espeak-ng/espeak-ng/"
-  url "https://github.com/espeak-ng/espeak-ng/archive/1.50.tar.gz"
+  homepage "https://github.com/nelsonlove/espeak-ng/"
+  url "https://github.com/nelsonlove/espeak-ng/archive/1.50.tar.gz"
   sha256 "5ce9f24ee662b5822a4acc45bed31425e70d7c50707b96b6c1603a335c7759fa"
-  head "https://github.com/espeak-ng/espeak-ng.git"
+  head "https://github.com/nelsonlove/espeak-ng.git"
 
   option "with-docs", "Build documentation"
   option "with-mbrola", "Enable MBROLA voice support"
@@ -15,8 +15,8 @@ class EspeakNg < Formula
   depends_on "automake" => :build
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
-  depends_on "anarchivist/espeak-ng/pcaudiolib" => :recommended
-  depends_on "anarchivist/espeak-ng/waywardgeek-sonic" => :recommended
+  depends_on "nelsonlove/espeak-ng/pcaudiolib" => :recommended
+  depends_on "nelsonlove/espeak-ng/waywardgeek-sonic" => :recommended
 
   conflicts_with "espeak", because: "espeak-ng also provides espeak and speak binaries"
 
--- Formula/pcaudiolib.rb
+++ Formula/pcaudiolib.rb
@@ -1,7 +1,7 @@
 class Pcaudiolib < Formula
   desc "Portable C Audio Library"
-  homepage "https://github.com/espeak-ng/pcaudiolib/"
-  url "https://github.com/espeak-ng/pcaudiolib/archive/1.1.tar.gz"
+  homepage "https://github.com/nelsonlove/pcaudiolib/"
+  url "https://github.com/nelsonlove/pcaudiolib/archive/1.1.tar.gz"
   sha256 "699a5a347b1e12dc5b122e192e19f4db01621826bf41b9ebefb1cbc63ae2180b"
 
   depends_on "autoconf" => :build
